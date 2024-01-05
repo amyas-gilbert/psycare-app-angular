3718f45157a89275412ac605b33c73b20453c70c
Amyas Gilbert <hamyas@gmail.com>
2024-01-05 17:42:24 +0000

    whatever

---
+++ src/app/festivals/festivals.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-festivals',
+  templateUrl: './festivals.component.html',
+  styleUrl: './festivals.component.scss'
+})
+export class FestivalsComponent {
+
+}
