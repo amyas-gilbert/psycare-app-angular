f439fc72aaa07bb30d89f99a65b07e764f3da1ce
Amyas Gilbert <hamyas@gmail.com>
2024-01-05 19:15:36 +0000

    festivals comp

--- src/app/festivals/festivals.component.ts
+++ src/app/festivals/festivals.component.ts
@@ -5,6 +5,13 @@ import { Component } from '@angular/core';
   templateUrl: './festivals.component.html',
   styleUrl: './festivals.component.scss'
 })
+
 export class FestivalsComponent {
+  festivals = [
+    {name: 'Noisily', location: 'Leicestershire', website: 'https://noisilyfestival.com/'},
+    {name: 'SGP', location: 'Huntingdon', website: 'https://www.secretgardenparty.com/'},
+    {name: 'Wonkfest', location: 'Prangton', website: '#'},
+    {name: 'Scrapness', location: 'Fuckedshire', website: '#'},
+  ]
 
 }
